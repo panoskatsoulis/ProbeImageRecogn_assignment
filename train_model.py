8d1a6fd55cd924364e37077ae5de1b52487472ec
kpanos <kpanos.pk@gmail.com>
2025-01-13 05:37:02 +0100

    acc func

--- train_model.py
+++ train_model.py
@@ -9,6 +9,9 @@ model.cpu()
 
 from dataset import train_data_loader, test_data_loader
 
+def acc_func(real, test):
+    return (torch.eq(real, test).sum().item() / len(test)) * 100
+
 loss_func = nn.MSELoss()  # Mean Squared Error loss for regression tasks
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 num_epochs = 5
@@ -31,10 +34,12 @@ for epoch in range(num_epochs):
     # test it
     model.eval()
     test_loss = 0.
+    accuracy = 0.
     with torch.inference_mode():
         for data_in, data_out in tqdm(test_data_loader):
             test_out = model(data_in)
             loss = loss_func(test_out, data_out)
             test_loss += loss.item()
-    
-    print(f'Finished epoch [{epoch+1}/{num_epochs}], Test Loss: {test_loss/len(test_data_loader)}')
+            accuracy += acc_func(data_out, test_out)
+
+    print(f'Finished epoch [{epoch+1}/{num_epochs}], Test Loss: {test_loss/len(test_data_loader)}, Test Acc: {accuracy/len(test_data_loader)}')
